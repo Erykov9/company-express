bf3e71956b57af9ee11236dbe4bd77a5b70d6932
Erykov9 <szczepanekeryk@gmail.com>
2022-11-05 00:21:15 +0100

    fix  server bug

--- server.js
+++ server.js
@@ -14,14 +14,12 @@ app.use('/api', testimonials);
 app.use('/api', seats);
 app.use('/api', concerts);
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/client/build/index.html'));
-});
-
-
-
 app.use((req, res) => {
   res.status(404).send({message: 'NOT FOUND 404'});
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
 });
 
 app.listen(process.env.PORT || 8000, () => {
