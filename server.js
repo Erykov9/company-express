77465b1d02a8264346995bb1b59cd84a61729d3d
Erykov9 <szczepanekeryk@gmail.com>
2022-11-05 01:13:02 +0100

    fix  server bugs

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const concerts = require('./routes/concerts.routes');
 const cors = require('cors');
 const path = require('path');
 const app = express();
-app.use(express.static(path.join(__dirname, '/client/build')));
 
 app.use(cors())
 app.use(express.urlencoded({extended: true}));
@@ -13,14 +12,17 @@ app.use(express.json());
 app.use('/api', testimonials);
 app.use('/api', seats);
 app.use('/api', concerts);
+app.use(express.static(path.join(__dirname, '/client/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
 
 app.use((req, res) => {
   res.status(404).send({message: 'NOT FOUND 404'});
 });
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/client/build/index.html'));
-});
+
 
 app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port: 8000');
